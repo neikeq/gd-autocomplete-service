99ea1c4a18fb82199f0d366d90163da1cd27a9d5
Ignacio Etcheverry <ignalfonsore@gmail.com>
2016-03-06 18:05:14 +0100

    SCsub: add sources only if tools and gdscript are enabled enabled in env

--- autocomplete_service/register_types.cpp
+++ autocomplete_service/register_types.cpp
@@ -1,12 +1,16 @@
 /* register_types.cpp */
 #include "register_types.h"
 
-#ifdef TOOLS_ENABLED
+#if defined(TOOLS_ENABLED) && defined(TOOLS_ENABLED)
+#define AUTOCOMPLETE_SERVICE
+#endif
+
+#ifdef AUTOCOMPLETE_SERVICE
 #include "completion_service_plugin.h"
 #endif
 
 void register_autocomplete_service_types() {
-#ifdef TOOLS_ENABLED
+#ifdef AUTOCOMPLETE_SERVICE
 	EditorPlugins::add_by_type<CompletionServicePlugin>();
 #endif
 }
